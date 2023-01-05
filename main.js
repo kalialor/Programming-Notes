e940a8350ee232e99bd380a26a74b82f63c9d411
Kalia Lor <kalialorSDE@gmail.com>
2023-01-05 14:42:56 -0500

    new notes

---
+++ main.js
@@ -0,0 +1,12 @@
+let input = document.querySelector('input');
+//
+//input.addEventListener('keyup', (e) => {
+//    if(e.keyCode === 13) {
+//        console.log(e.target.value);
+//    }
+//})
+
+
+input.addEventListener('click', (e) => {
+    console.log("YEEET")
+})
