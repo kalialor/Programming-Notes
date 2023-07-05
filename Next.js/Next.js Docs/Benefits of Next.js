9505a2475a98cae77e0ba6723efd1591ef28cce8
Kalia Lor <kalialorSDE@gmail.com>
2023-07-05 18:11:47 -0400

    react notes

---
+++ Next.js/Next.js Docs/Benefits of Next.js
@@ -0,0 +1,15 @@
+Next.js is a tool that helps build websites and web applications. Here are some benefits of using Next.js:
+
+1. **Easy setup**: Next.js makes it simple to set up a project and start building right away. It provides a lot of useful features out of the box, so you don't have to worry about configuring everything from scratch.
+
+2. **Server-side rendering (SSR)**: Next.js excels at server-side rendering, which means it can generate the webpages on the server and send them to your browser. This results in faster page loads and better search engine optimization (SEO) because search engines can read the fully rendered content.
+
+3. **Client-side rendering (CSR)**: Next.js also supports client-side rendering, which means it can render parts of your website on the client side (in your browser) after the initial page load. This allows for dynamic and interactive user experiences.
+
+4. **Static site generation (SSG)**: Next.js supports static site generation, which means it can generate the entire website as static HTML files in advance. This is great for websites with content that doesn't change frequently. It improves performance and reduces the need for server resources.
+
+5. **Developer-friendly features**: Next.js provides a variety of developer-friendly features, such as automatic code splitting, hot module replacement, and built-in CSS and Sass support. It also has a great development environment with fast refresh, allowing you to see your changes instantly.
+
+6. **Excellent community and ecosystem**: Next.js has a vibrant community with plenty of resources, tutorials, and libraries available. It's backed by a well-established company, Vercel, which provides hosting and deployment services specifically tailored for Next.js projects.
+
+These are just a few of the benefits of using Next.js. It's a powerful tool that simplifies web development and offers flexibility depending on your project's needs.
---
+++ Next.js/Next.js from Development to Deployment: Build a Music Event Website [Video]/What is Next.js
@@ -0,0 +1,27 @@
+https://subscription.packtpub.com/video/web-development/9781801814706/p1/video1_2/what-is-nextjs
+
+Starting A Project using Next.js:
+
+npx create-next-app ./desktop/projects
+
+
+
+Next.js allows for server side rendering of your React application. You can also use it to create static websites. It is an alternative to Gatsby. 
+
+
+
+What is server-side rendering? 
+It allows the application to be rendered on the server on the first page load. This gives a lot of benefits such as data fetching methods, APi Routes, better performance, and great for SEO because search engines can actually see your content. 
+
+
+Next.js is both front & back-end
+It does have backend capabilities to handle requeest response, you can create your own API routes, and so on. 
+
+Vercel is the creator of Next
+
+
+Next allows for easy page routing
+There's no need to install React Router DOM and manually create your routes. You simply put compoents into a Pages folder. 
+
+
+Next also has TypeScript and Sass. If you want to use Typescript, you can rename your 'js' files to 'ts'
